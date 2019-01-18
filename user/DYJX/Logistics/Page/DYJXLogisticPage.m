38f984b92f47e294ac335ddd8de6a2954d01d60c
岩  熊 <yanxiong@yandeMacBook-PRO.lan>
2019-01-18 18:23:42 +0800

    用户类型分析

--- user/DYJX/Logistics/Page/DYJXLogisticPage.m
+++ user/DYJX/Logistics/Page/DYJXLogisticPage.m
@@ -38,7 +38,7 @@
     [self initNavigation];
     [self initSubViews];
     [self initCollectionView];
-    [self setUpTitleView];
+//    [self setUpTitleView];
 
     [self.viewModel.requestDic setObject:self.IdentityModel.Id forKey:@"CertificateId"];
     [self.viewModel getMyAppsSuccess:^{
@@ -255,14 +255,14 @@
         titleView = [[NIMKitTitleView alloc] initWithFrame:CGRectZero];
         self.navigationItem.titleView = titleView;
 
-        titleView.titleLabel.text = @"选择";
+        titleView.titleLabel.text = @"企业简讯";
     }
 
     [titleView sizeToFit];
 }
 
 - (void)initNavigation{
-    self.title = @"达意简讯";
+    self.title = @"企业简讯";
 
     [self.navigationItem.leftBarButtonItem setCustomView:[UIView new]];
 
